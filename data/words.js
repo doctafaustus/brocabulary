d5c1198ef994f08d8ceb11a0b178be258500ed7e
Bill Coloe <bill@lovevery.com>
2022-04-03 16:38:42 -0600

    Add eslint rule to allow unescaped entitites

--- data/words.js
+++ data/words.js
@@ -1,14 +1,14 @@
 const words = [
   {
     name: 'menagerie',
-    date: '3/26/2022',
+    date: '4/2/2022',
     definition: 'a collection of wild animals kept in captivity for exhibition.',
     sentence: 'It seems the creature escaped from the private <b>menagerie</b>.',
     image: 'https://i.ibb.co/CsmG2Wd/1-Menagerie-transparent.png'
   },
   {
     name: 'extol',
-    date: '3/27/2022',
+    date: '4/3/2022',
     definition: 'praise enthusiastically.',
     sentence: 'The judges <b>extolled</b> the dish from the young home cook',
     image: 'https://i.ibb.co/hZTVSFt/2-Extol-transparent.png'
