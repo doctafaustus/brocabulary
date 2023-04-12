f2ed97cb29929d346b9403805055ce387e95f83d
doctafaustus <clowd150@gmail.com>
2023-04-12 11:44:39 -0500

    Test randomId for Next

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '/styles/_global.css';
 
+const randomId = Math.floor(Math.random() * 1000);
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <h1>randomId: {randomId}</h1>
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
